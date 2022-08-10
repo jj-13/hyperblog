c288b337fc79fd273c3320bf2b482b5636a28819
jj-13 <jj.rosero@hotmail.com>
2022-08-09 19:00:53 -0500

    se agregaron funciones

--- b.py
+++ b.py
@@ -3,3 +3,14 @@
 l2 = [i for i in range(5)]
 sum = 0
 
+for i in l2:
+    sum += i
+
+def splitstr(word):
+    v1 = [i for i in word]
+
+    return v1
+
+v2 = splitstr('hello')
+print(v2)
+print(l2)
