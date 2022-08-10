e77eb09e90756a0e6b1c360655f77f8d5141eec6
jj-13 <jj.rosero@hotmail.com>
2022-08-09 22:30:48 -0500

    se creo funcion para generar los cambios

--- c.py
+++ c.py
@@ -3,9 +3,10 @@ l4 = [i for i in range(6)]
 l4[3] = 100
 del l4[0] #instruccion del
 
-print(l4)
-print(l4[:1])
-print(l4[0])
-print(l4[-1:])
-print(l4[-1])
-print(l4[-2])
+def d1():
+    print(l4)
+    print(l4[:1])
+    print(l4[0])
+    print(l4[-1:])
+    print(l4[-1])
+    print(l4[-2])
